7c906474a1ebb6ab371f531a1d503d6f63f815cb
AhmadQadoura <ahmad.h.qadoura@gmail.com>
2023-03-28 13:17:00 +0300

    backup

--- expense-tracker/App.js
+++ expense-tracker/App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import ManageExpenses from './components/ManageExpenses'
+import RecentExpenses from './components/RecentExpenses'
+import AllExpenses from  './components/AllExpenses'
+import { GlobalStyles } from './constanse/Styles';
+const Stack = createNativeStackNavigator()
+const Bottom= createBottomTabNavigator()
 
-export default function App() {
+function ExpensesOverView(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Bottom.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500
+      }}
+    >
+      <Bottom.Screen  name="RecentExpenses" component={RecentExpenses} options={{
+        title:'Recent Expenses'
+      }} />
+      <Bottom.Screen name="AllExpenses" component={AllExpenses} />
+    </Bottom.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+export default function App() {
+
+
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverView" component={ExpensesOverView} options={{headerShown:false}} />
+          <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
